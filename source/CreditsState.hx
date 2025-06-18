f8a49102bdbeca3f97eb3091f1fea91bb968ccb6
Joalor64 <rhgarcon@gmail.com>
2025-06-18 17:08:36 -0400

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -138,8 +138,6 @@ class CreditsState extends MusicBeatState
 			}
 			if (controls.BACK)
 			{
-				if(colorTween != null)
-					colorTween.cancel();
 				FlxG.sound.play(Paths.sound('cancelMenu'));
 				MusicBeatState.switchState(new MainMenuState());
 			}
