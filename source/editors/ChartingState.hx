53b39e06e90bb187164747885d2d172195243d36
Joalor64 <rhgarcon@gmail.com>
2025-06-20 16:20:55 -0400

    Update ChartingState.hx

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -425,7 +425,7 @@ class ChartingState extends MusicBeatState
 		var loadAutosaveBtn:FlxButton = new FlxButton(reloadSongJson.x, reloadSongJson.y + 30, 'Load Autosave', function()
 		{
 			PlayState.SONG = Song.parseJSONshit(FlxG.save.data.autosave);
-			MusicBeatState.resetState();
+			FlxG.resetState();
 		});
 
 		var loadEventJson:FlxButton = new FlxButton(loadAutosaveBtn.x, loadAutosaveBtn.y + 30, 'Load Events', function()
