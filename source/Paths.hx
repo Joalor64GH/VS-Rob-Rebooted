e0cccdd0a5c72f2991287267eba4968ed411e5c3
Joalor64 <rhgarcon@gmail.com>
2025-06-21 11:49:01 -0400

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -107,7 +107,7 @@ class Paths
 
 	static public var currentModDirectory:String = '';
 
-	public static function getPath(file:String)
+	public static function getPath(file:String = '')
 	{
 		return 'assets/$file';
 	}
